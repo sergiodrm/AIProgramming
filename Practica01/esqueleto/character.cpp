98022c6a86b5c0ae42770341cd37cc32f4ce885f
sergiodrm <47793261+sergiodrm@users.noreply.github.com>
2021-03-10 21:56:29 +0100

    Feature: seek steering implemented. Basics draw debug.

--- Practica01/esqueleto/character.cpp
+++ Practica01/esqueleto/character.cpp
@@ -6,80 +6,95 @@
 
 Character::Character() : mLinearVelocity(0.0f, 0.0f), mAngularVelocity(0.0f)
 {
-	RTTI_BEGIN
-		RTTI_EXTEND (MOAIEntity2D)
-	RTTI_END
+  RTTI_BEGIN
+  RTTI_EXTEND(MOAIEntity2D)
+  RTTI_END
 }
 
 Character::~Character()
-{
-
-}
+{}
 
 void Character::OnStart()
 {
-    ReadParams("params.xml", mParams);
+  ReadParams("params.xml", mParams);
 }
 
 void Character::OnStop()
-{
-
-}
+{}
 
 void Character::OnUpdate(float step)
 {
+  MOAIEntity2D::OnUpdate(step);
+
+  // Get direction
+  USVec3D position = GetLoc();
+  USVec3D destination = mParams.targetPosition;
+  mDirection = destination - position;
+  mDirection.SetLength(1.f);
+
+  // Calculate velocity
+  mSteering = mDirection * mParams.max_velocity;
+  // Calculate acceleration
+  mSteering = mSteering - mLinearVelocity;
+  mSteering.Clamp(USVec2D(mParams.max_acceleration, mParams.max_acceleration));
+  mLinearVelocity += mSteering;
+  mLinearVelocity.Clamp(USVec2D(mParams.max_velocity, mParams.max_velocity));
+
+  // Update position
+  position = position + mLinearVelocity * step;
+  SetLoc(position);
 }
 
 void Character::DrawDebug()
 {
-	MOAIGfxDevice& gfxDevice = MOAIGfxDevice::Get();
-	gfxDevice.SetPenColor(0.0f, 0.0f, 1.0f, 0.5f);
+  MOAIGfxDevice& gfxDevice = MOAIGfxDevice::Get();
+  gfxDevice.SetPenColor(0.7f, 0.2f, 0.2f, 1.f);
 
-	vector<USMetaVec2D<float>> targetPolygon;
-	targetPolygon.push_back(USMetaVec2D<float>(mParams.targetPosition.mX - 10.f, mParams.targetPosition.mY - 10.f));
-	targetPolygon.push_back(USMetaVec2D<float>(mParams.targetPosition.mX + 10.f, mParams.targetPosition.mY - 10.f));
-	targetPolygon.push_back(USMetaVec2D<float>(mParams.targetPosition.mX + 10.f, mParams.targetPosition.mY + 10.f));
-	targetPolygon.push_back(USMetaVec2D<float>(mParams.targetPosition.mX - 10.f, mParams.targetPosition.mY + 10.f));
-	MOAIDraw::DrawPoint(0.0f, 0.0f);
-	MOAIDraw::DrawPolygon(targetPolygon);
+  // Draw target
+  MOAIDraw::DrawPoint(mParams.targetPosition.mX, mParams.targetPosition.mY);
+  MOAIDraw::DrawEllipseOutline(mParams.targetPosition.mX, mParams.targetPosition.mY, mParams.dest_radius,
+                               mParams.dest_radius, 10);
+
+  // Draw debug vars
+  MOAIDraw::DrawLine(GetLoc().mX, GetLoc().mY, mDirection.mX, mDirection.mY);
+  gfxDevice.SetPenColor(0.7f, 0.7f, 0.2f, 1.f);
+  MOAIDraw::DrawLine(GetLoc().mX, GetLoc().mY, mLinearVelocity.mX, mLinearVelocity.mY);
+  gfxDevice.SetPenColor(0.2f, 0.2f, 1.f, 1.f);
+  MOAIDraw::DrawLine(GetLoc().mX, GetLoc().mY, mSteering.mX, mSteering.mY);
 }
-
-
-
 
 
 // Lua configuration
 
 void Character::RegisterLuaFuncs(MOAILuaState& state)
 {
-	MOAIEntity2D::RegisterLuaFuncs(state);
-	
-	luaL_Reg regTable [] = {
-		{ "setLinearVel",			_setLinearVel},
-		{ "setAngularVel",			_setAngularVel},
-		{ NULL, NULL }
-	};
+  MOAIEntity2D::RegisterLuaFuncs(state);
 
-	luaL_register(state, 0, regTable);
+  luaL_Reg regTable[] = {
+      {"setLinearVel", _setLinearVel},
+      {"setAngularVel", _setAngularVel},
+      {nullptr, nullptr}
+    };
+
+  luaL_register(state, nullptr, regTable);
 }
 
 int Character::_setLinearVel(lua_State* L)
 {
-	MOAI_LUA_SETUP(Character, "U")
-	
-	float pX = state.GetValue<float>(2, 0.0f);
-	float pY = state.GetValue<float>(3, 0.0f);
-	self->SetLinearVelocity(pX, pY);
-	return 0;	
+  MOAI_LUA_SETUP(Character, "U")
+
+  float pX = state.GetValue<float>(2, 0.0f);
+  float pY = state.GetValue<float>(3, 0.0f);
+  self->SetLinearVelocity(pX, pY);
+  return 0;
 }
 
 int Character::_setAngularVel(lua_State* L)
 {
-	MOAI_LUA_SETUP(Character, "U")
-	
-	float angle = state.GetValue<float>(2, 0.0f);
-	self->SetAngularVelocity(angle);
+  MOAI_LUA_SETUP(Character, "U")
 
-	return 0;
+  float angle = state.GetValue<float>(2, 0.0f);
+  self->SetAngularVelocity(angle);
+
+  return 0;
 }
-	
--- Practica01/esqueleto/character.h
+++ Practica01/esqueleto/character.h
@@ -4,37 +4,46 @@
 #include <moaicore/MOAIEntity2D.h>
 #include <params.h>
 
-class Character: public MOAIEntity2D
+class Character : public MOAIEntity2D
 {
 public:
-    DECL_LUA_FACTORY(Character)
+  DECL_LUA_FACTORY(Character)
 protected:
-	virtual void OnStart();
-	virtual void OnStop();
-	virtual void OnUpdate(float step);
+  void OnStart() override;
+  void OnStop() override;
+  void OnUpdate(float step) override;
 public:
-	virtual void DrawDebug();
+  void DrawDebug() override;
 
-	Character();
-	~Character();
-	
-	void SetLinearVelocity(float x, float y) { mLinearVelocity.mX = x; mLinearVelocity.mY = y;}
-	void SetAngularVelocity(float angle) { mAngularVelocity = angle;}
-	
-	USVec2D GetLinearVelocity() const { return mLinearVelocity;}
-	float GetAngularVelocity() const { return mAngularVelocity;}
+  Character();
+  ~Character();
+
+  void SetLinearVelocity(float x, float y)
+  {
+    mLinearVelocity.mX = x;
+    mLinearVelocity.mY = y;
+  }
+
+  void SetAngularVelocity(float angle) { mAngularVelocity = angle; }
+
+  USVec2D GetLinearVelocity() const { return mLinearVelocity; }
+  float GetAngularVelocity() const { return mAngularVelocity; }
 private:
-	USVec2D mLinearVelocity;
-	float mAngularVelocity;
-	
-	Params mParams;
-	
-	// Lua configuration
+  USVec2D mLinearVelocity;
+  float mAngularVelocity;
+
+  Params mParams;
+
+  /// Draw debug vars
+  USVec2D mDirection;
+  USVec2D mSteering;
+
+  // Lua configuration
 public:
-	virtual void RegisterLuaFuncs(MOAILuaState& state);
+  void RegisterLuaFuncs(MOAILuaState& state) override;
 private:
-	static int _setLinearVel(lua_State* L);
-	static int _setAngularVel(lua_State* L);
+  static int _setLinearVel(lua_State* L);
+  static int _setAngularVel(lua_State* L);
 };
 
 #endif
